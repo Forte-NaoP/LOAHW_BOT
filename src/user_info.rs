81a5d2a54c9c898b6505e0344462bf32409176e5
BJH9750 <bae9750@gmail.com>
2022-09-24 18:46:11 +0900

    delete CharInfo

--- src/user_info.rs
+++ src/user_info.rs
@@ -8,17 +8,8 @@ pub struct UserInfo {
     user_character: HashMap<String, u32>,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct CharInfo {
-    char_name: String,
-    char_lv: u32,
-}
-
 impl UserInfo {
-    pub fn new(user_name: String, user_character: Vec<CharInfo>) -> UserInfo {
-        let user_character:HashMap<String, u32> = user_character.iter().map(|character| {
-            (character.char_name.to_string(), character.char_lv)
-        }).collect();
+    pub fn new(user_name: String, user_character: HashMap<String, u32>) -> UserInfo {
         UserInfo { user_name, user_character }
     }
 
@@ -27,36 +18,9 @@ impl UserInfo {
     }
 
     pub fn to_json(&self) -> String {
-        let char_info_slice:Vec<String> = self.user_character.iter().map(
-            |char_info| {
-                format!("{}:{}", char_info.0, char_info.1)
-            }
-        ).collect();
-        
         json!({
             "user_name": self.user_name,
-            "user_character": char_info_slice,
+            "user_character": self.user_character,
         }).to_string()
-    }
-}
-
-impl CharInfo {
-    pub fn new(char_name: String, char_lv: u32) -> CharInfo {
-        CharInfo { char_name, char_lv }
-    }
-    pub fn char_name(&self) -> &str {
-        &self.char_name
-    }
-
-    pub fn char_name_mut(&mut self) -> &mut String {
-        &mut self.char_name
-    }
-
-    pub fn char_lv(&self) -> u32 {
-        self.char_lv
-    }
-    
-    pub fn set_char_lv(&mut self, lv: u32) {
-        self.char_lv = lv;
     }
 }

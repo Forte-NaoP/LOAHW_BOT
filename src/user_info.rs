4bd76b11fc74f46e8e6071a22163705e69ffef11
BJH9750 <bae9750@gmail.com>
2022-09-24 15:49:35 +0900

    Use HashMap instead of Vec

--- src/user_info.rs
+++ src/user_info.rs
@@ -1,22 +1,44 @@
-pub struct user_info {
+use serde_json::{self, json};
+use std::collections::HashMap;
+
+pub struct UserInfo {
     user_name: String,
-    user_character: Vec<char_info>,
+    user_character: HashMap<String, u32>,
 }
 
-pub struct char_info {
+pub struct CharInfo {
     char_name: String,
     char_lv: u32,
 }
 
-impl user_info {
-    pub fn new(user_name: String, user_character: Vec<char_info>) -> user_info {
-        user_info { user_name, user_character }
+impl UserInfo {
+    pub fn new(user_name: String, user_character: Vec<CharInfo>) -> UserInfo {
+        let user_character:HashMap<String, u32> = user_character.iter().map(|character| {
+            (character.char_name.to_string(), character.char_lv)
+        }).collect();
+        UserInfo { user_name, user_character }
     }
-    
+
+    pub fn user_name(&self) -> &str {
+        &self.user_name
+    }
+
+    pub fn to_json(&self) -> String {
+        let char_info_slice:Vec<String> = self.user_character.iter().map(
+            |char_info| {
+                format!("{}:{}", char_info.0, char_info.1)
+            }
+        ).collect();
+        
+        json!({
+            "user_name": self.user_name,
+            "user_character": char_info_slice,
+        }).to_string()
+    }
 }
 
-impl char_info {
-    pub fn new(char_name: String, char_lv: u32) -> char_info {
-        char_info { char_name, char_lv }
+impl CharInfo {
+    pub fn new(char_name: String, char_lv: u32) -> CharInfo {
+        CharInfo { char_name, char_lv }
     }
 }

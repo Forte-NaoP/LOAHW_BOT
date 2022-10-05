2333f463df6617fea0c95e4d36763a5b73fd9557
BJH9750 <bae9750@gmail.com>
2022-10-06 04:09:01 +0900

    Convert poise to serenity

--- src/commands.rs
+++ src/commands.rs
@@ -1,42 +1,123 @@
-use std::{fmt::format, collections::HashMap};
-
-use crate::{Context, Error, database_handler, user_info::{UserInfo, CharInfo, self}};
-use poise::serenity_prelude::{self as serenity, User};
+use std::collections::HashMap;
+use crate::{database_handler, user_info::*, DBContainer};
 use regex::Regex;
 
-#[poise::command(prefix_command, owners_only, hide_in_help)]
-pub async fn shutdown(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.framework()
-        .shard_manager()
-        .lock()
-        .await
-        .shutdown_all()
-        .await;
+use rusqlite::{Result, params};
+use tokio_rusqlite::Connection as Connection;
+
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::prelude::{GatewayIntents, ClientError};
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework, CommandError, Args
+};
+use serenity::model::{
+    prelude::{Message, Reaction, ReactionType, Ready},
+    application::component::{SelectMenu, ComponentType, SelectMenuOption},
+    application::interaction::InteractionResponseType,
+};
+
+
+#[command]
+#[aliases("초기화")]
+pub async fn user_init(ctx: &Context, msg: &Message) -> CommandResult {
+    database_handler::user_delete(ctx.data.write().await.get_mut::<DBContainer>().unwrap(), msg.author.tag()).await?;
+    msg.channel_id.say(&ctx.http, format!("reset data of {}", msg.author.tag())).await?;
     Ok(())
 }
 
-/// ~등록 character1 class lv  [character2 class lv] [--all]
-#[poise::command(prefix_command)]
-pub async fn 등록(
-    ctx: Context<'_>,
-    msg: Vec<String>,
-) -> Result<(), Error> {
-    
-    let str = msg.join(" ");
-    let user_info: UserInfo = user_info_from_msg(&ctx.author().name, &str);
+// ~등록 character1 class lv  [character2 class lv] [--all]
+#[command]
+#[aliases("등록")]
+pub async fn register(
+    ctx: &Context,
+    msg: &Message,
+    #[delimiters(Delimiter::Single(' '))] 
+    args: Args
+) -> CommandResult {
 
-    let mut response = String::new();
-    for (k, v) in user_info.user_character().iter() {
-        response += &format!("name: {}, class: {}, lv: {}\n", k, v.0, v.1).to_string();
-    }
+    let str = args.raw().collect::<Vec<&str>>().join(" ");
+    let user_info = user_info_from_msg(&msg.author.tag(), &str);
+
+    let response = build_response(user_info.user_character());
 
     if response.is_empty() {
-        ctx.say("Parsing Failed").await?;
+        msg.channel_id.say(&ctx.http, "Parsing Failed").await?;
     } else {
-        ctx.say(response).await?;
+        let result = database_handler::user_register(&ctx.data.write().await.get_mut::<DBContainer>().unwrap(), user_info).await;
+        match result {
+            Ok(()) => {
+                msg.channel_id.say(&ctx.http, response).await?;
+            },
+            Err(why) => {
+                msg.channel_id.say(&ctx.http, "Register Failed").await?;
+            }
+        }
     }
 
-    database_handler::user_register(&ctx.data().conn, user_info).await?;
+    Ok(())
+}
+
+#[command]
+#[aliases("수정", "갱신", "추가")]
+pub async fn update(
+    ctx: &Context,
+    msg: &Message,
+    #[delimiters(Delimiter::Single(' '))] 
+    args: Args
+) -> CommandResult {
+
+    let str = args.raw().collect::<Vec<&str>>().join(" ");
+    let user_info = user_info_from_msg(&msg.author.tag(), &str);
+
+    let response = build_response(user_info.user_character());
+
+    if response.is_empty() {
+        msg.channel_id.say(&ctx.http, "Parsing Failed").await?;
+    } else {
+        let result = database_handler::user_update(&ctx.data.write().await.get_mut::<DBContainer>().unwrap(), user_info).await;
+        match result {
+            Ok(()) => {
+                msg.channel_id.say(&ctx.http, response).await?;
+            },
+            Err(why) => {
+                msg.channel_id.say(&ctx.http, "Update Failed").await?;
+            }
+        }
+    }
+    Ok(())
+}
+
+#[command]
+#[aliases("삭제")]
+pub async fn delete(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let name = args.raw().collect::<Vec<&str>>()[0];
+    let result = database_handler::character_delete(
+        &ctx.data.write().await.get_mut::<DBContainer>().unwrap(), 
+        msg.author.tag().to_string(), 
+        name.to_string()
+    ).await;
+    match result {
+        Ok(()) => {
+            msg.channel_id.say(&ctx.http, format!("delete character {} of {}", name, &msg.author.tag())).await?;
+        },
+        Err(why) => {
+            msg.channel_id.say(&ctx.http, "Delete Failed").await?;
+        }
+    }
+    Ok(())
+}
+
+#[command]
+#[aliases("조회")]
+pub async fn query(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let result = database_handler::user_query(
+        &ctx.data.write().await.get_mut::<DBContainer>().unwrap(),
+        msg.author.tag().to_string(),
+    ).await?;
+
+    msg.channel_id.say(&ctx.http, format!("{}\n{}", &msg.author.tag(), result)).await?;
 
     Ok(())
 }
@@ -44,11 +125,23 @@ pub async fn 등록(
 fn user_info_from_msg(name: &String, msg: &String) -> UserInfo {
 
     let re = Regex::new(r"((?P<name>\w+?)\s+?(?P<class>\w+?)\s+?(?P<lv>\d+))").unwrap();
-    let mut charinfo: user_info::CharInfo = HashMap::new();
+    let mut charinfo: CharInfo = HashMap::new();
 
     for caps in re.captures_iter(msg) {
-        charinfo.insert(caps["name"].to_string(), (caps["class"].to_string(), caps["lv"].parse::<u32>().unwrap(), 0));
+        charinfo.insert(caps["name"].to_string(),
+        CharData::new(
+            caps["class"].to_string(),
+            caps["lv"].parse::<u32>().unwrap(),
+        ));
     }
 
     UserInfo::new(name.to_string(), charinfo)
+}
+
+fn build_response(charinfo: &CharInfo) -> String {
+    let mut response = String::new();
+    for (k, v) in charinfo.iter() {
+        response += &format!("name: {}, class: {}, lv: {}\n", k, v.class(), v.lv()).to_string();
+    }
+    response
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,84 @@
-use rusqlite::{Connection, Result};
+
+use std::{env, vec};
+use std::{time::Duration, sync::Arc};
+
+use rusqlite::{Result, params};
+use tokio_rusqlite::Connection as Connection;
+
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::prelude::*;
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework, CommandError, Args
+};
+use serenity::model::{
+    prelude::{Message, Reaction, ReactionType, Ready},
+    application::component::{SelectMenu, ComponentType, SelectMenuOption},
+    application::interaction::InteractionResponseType,
+};
 
 mod user_info;
 mod database_handler;
+mod commands;
+mod gold_calc;
 
-fn main() -> Result<()>{
-    let conn = Connection::open("practice.db")?;
+use commands::*;
+
+
+#[group]
+#[commands(user_init, register, update, delete, query)]
+
+struct General;
+
+struct DBContainer;
+
+impl TypeMapKey for DBContainer {
+    type Value = Connection;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()>{
+
+    let conn = Connection::open("user.db").await?;
+
+    database_handler::initialize(&conn).await?;
+
+    let framework = StandardFramework::new().configure(|c| c.prefix("~")).group(&GENERAL_GROUP);
+
+    let token = std::env::var("DISCORD_TOKEN").unwrap();
+    let intents = GatewayIntents::GUILDS
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::GUILD_MESSAGE_REACTIONS
+        | GatewayIntents::GUILD_MEMBERS
+        | GatewayIntents::MESSAGE_CONTENT
+    ;
+
+    let mut client = Client::builder(
+        token, 
+        intents
+    )
+    .event_handler(Handler)
+    .framework(framework)
+    .await
+    .expect("Error creating client");
+    {
+        let mut data = client.data.write().await;
+        data.insert::<DBContainer>(conn);
+    }
+    
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 
     Ok(())
 }

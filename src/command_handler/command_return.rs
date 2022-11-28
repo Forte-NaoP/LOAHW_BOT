0a37dafeb875c06f1eef40d2a16d009b28b01c09
BJH9750 <bae9750@gmail.com>
2022-11-28 22:44:06 +0900

    Feat: CommandReturn 타입 추가

--- src/command_handler/command_return.rs
+++ src/command_handler/command_return.rs
@@ -1,4 +1,9 @@
+use crate::embed_pages;
+use serenity::builder::CreateEmbed;
+
 pub enum CommandReturn {
     String(String),
+    SingleEmbed(CreateEmbed),
+    EmbedPages(embed_pages::EmbedPages),
     None,
 }

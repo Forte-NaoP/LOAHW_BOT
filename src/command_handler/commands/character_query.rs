190b5630f5cfc9caef90ca5f2b49ecddf0cb94f6
BJH9750 <bae9750@gmail.com>
2022-12-04 21:54:40 +0900

    Feat: 보유한 캐릭터의 군단장 숙제 현황 조회
    Embed와 SelectMenu로 보유한 캐릭터의 군단장 속제 현황 확인 가능

--- src/command_handler/commands/character_query.rs
+++ src/command_handler/commands/character_query.rs
@@ -1,15 +1,17 @@
 use serenity::{
     async_trait,
-    builder::CreateApplicationCommand,
+    builder::{CreateApplicationCommand, CreateActionRow, CreateEmbed, CreateSelectMenu, CreateSelectMenuOption, CreateSelectMenuOptions},
     client::{Context},
     framework::standard::{
         CommandResult, Args,
     },
     model::{
-        application::interaction::application_command::ApplicationCommandInteraction,
+        application::interaction::{
+            application_command::ApplicationCommandInteraction, InteractionResponseType,
+        },
         prelude::{
             Message,
-            interaction::application_command::{CommandDataOption, CommandDataOptionValue},
+            interaction::{application_command::{CommandDataOption, CommandDataOptionValue}, self},
             command::CommandOptionType,
         },
         user::User,
@@ -17,14 +19,19 @@ use serenity::{
 };
 
 use crate::{
-    database_handler, DBContainer, loa_contents::LOA_CONTENTS, user_info::*,
+    database_handler, DBContainer, loa_contents::{LOA_CONTENTS, CONTENTS_NAME}, user_info::*,
     command_handler::{
         command_handler::*,
         command_data::*,
-        command_return::{CommandReturn, BuildActionRow},
+        command_return::{CommandReturn, ControlInteraction},
     },
-    embed_pages,
+    utils::*,
 };
+
+use std::any::Any;
+use log::error;
+use std::time::Duration;
+use futures::StreamExt;
 
 struct CharacterQuery;
 
@@ -46,8 +53,8 @@ impl CommandInterface for CharacterQuery {
             command.user.tag(),
         ).await {
             Ok(result) => {
-                let pages = embed_pages::EmbedPages::from_user_info(result);
-                CommandReturn::EmbedPages(pages)
+                let pages = QueryResult::from_user_info(result);
+                CommandReturn::ControlInteraction(Box::new(pages))
             },
             Err(e) => CommandReturn::String("등록되지 않음".to_owned())
         }
@@ -63,3 +70,105 @@ impl CommandInterface for CharacterQuery {
     }
 }
 
+struct QueryResult {
+    pub pages: Vec<CreateEmbed>,
+}
+
+impl QueryResult {
+    pub fn from_user_info(userinfo: UserInfo) -> QueryResult {
+        let mut pages: Vec<(f64, CreateEmbed)> = vec![];
+
+        for (_name, _charinfo) in userinfo.user_character().iter() {
+            let mut embed = CreateEmbed::default();
+            if _charinfo.lv >= 1302.0 {
+                embed
+                    .title(format!("```{}```", _name))
+                    .description(format!("{} {}", _charinfo.class, _charinfo.lv))
+                    .fields(
+                        get_content_list(_charinfo)
+                    );
+                pages.push((_charinfo.lv, embed));
+            }
+        }
+
+        pages.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+        let pages = pages.iter().map(|(_, b)| b.to_owned()).collect::<Vec<_>>();
+
+        QueryResult { pages }
+    }
+}
+
+#[async_trait]
+impl ControlInteraction for QueryResult {
+    async fn control_interaction(
+        &self,
+        ctx: &Context, 
+        interaction: ApplicationCommandInteraction, 
+    ) -> Result<(), serenity::Error> {
+
+        let mut names = vec![];
+
+        for embed in self.pages.iter() {
+            names.push(embed.0.get("title").unwrap().as_str().unwrap().replace("`", ""));
+        }
+
+        if let Err(why) = interaction
+            .edit_original_interaction_response(&ctx.http, |interaction| {
+                interaction.set_embed(self.pages[0].to_owned());
+                interaction.components(|c| {
+                    c.create_action_row(|r| {
+                        r.add_select_menu(select_menu_from_vec(&names, 0))
+                    })
+                })
+            })
+            .await
+        {
+            error!("an error occured while adding select menus.");
+            return Err(why);
+        }
+
+        match interaction.get_interaction_response(&ctx.http).await {
+            Ok(msg) => {
+                let mut interaction_stream = msg
+                    .await_component_interactions(&ctx)
+                    .timeout(Duration::from_secs(60*5))
+                    .filter(move |f| {
+                        f.message.id == msg.id && f.member.as_ref().unwrap().user.id == interaction.user.id
+                    })
+                    .build();
+    
+                while let Some(select_option) = interaction_stream.next().await {
+                    if let Err(why) = select_option
+                        .create_interaction_response(&ctx.http, |r| {
+                            r.kind(InteractionResponseType::UpdateMessage)
+                                .interaction_response_data(|i| {
+                                    let idx = select_option.data.values[0].parse::<usize>().unwrap();
+                                    i.set_embed(
+                                        self.pages[idx].to_owned()
+                                    )
+                                    .components(|f| {
+                                        f.create_action_row(|f| {
+                                            f.add_select_menu(select_menu_from_vec(&names, idx))
+                                        })
+                                    })
+                                })
+                        })
+                        .await
+                    {
+                        error!("Couldn't set embed.");
+                        return Err(why);
+                    }
+                }
+            }
+            Err(why) => {
+                error!("Couldn't get message info from interaction.");
+                return Err(why);
+            }
+        }
+        Ok(())
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
